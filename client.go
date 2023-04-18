9bde470f2e5d9a54a54daf429440f81291ecd657
Steffen Vogel <post@steffenvogel.de>
2023-04-18 14:04:26 +0200

    Make package REUSE compliant

--- client.go
+++ client.go
@@ -1,11 +1,17 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
 package portmap
 
+// Client requests port mappings.
 type Client struct{}
 
+// NewClient creates a new client.
 func NewClient() (*Client, error) {
 	return &Client{}, nil
 }
 
+// Close closes a client.
 func (c *Client) Close() error {
 	return nil
 }
---
+++ portmap.go
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+// Package portmap implements a common API for various protocols
+// to configure port forwardings.
+package portmap
